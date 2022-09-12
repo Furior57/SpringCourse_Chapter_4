c03a30d072d1a1ec929e66d1b4ace5f17e0f7bc8
Furior57 <Furior572@gmail.com>
2022-09-12 08:54:03 +0300

    12.09.22 SpringREST_GET/api/employees/{id}

---
+++ src/main/java/com/rest/Lesson_59_Get_one_employee.java
@@ -0,0 +1,7 @@
+package com.rest;
+
+public class Lesson_59_Get_one_employee {
+    // В этой лекции мы реализуем функционал получения из базы одного работника по его id.
+    // Вспоминаем таблицу, запрос должен выглядеть так: GET/api/employees/{id}.
+    // Перейдем в MyRestController на 40 строку.
+}
--- src/main/java/com/rest/controller/MyRestController.java
+++ src/main/java/com/rest/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.rest.entity.Employee;
 import com.rest.service.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,6 @@ import java.util.List;
 // вставим в наш проект и пропишем корректные пути. Нам нет смысла писать заново то
 // что мы уже написали, логика работы будет точно такая же. Мы будем получать всех работников,
 // получать конкретного работника по id, обновлять и удалять данные о работнике, так же по id.
-
-
 @RestController
 // Ну и конечно определим @RequestMapping
 @RequestMapping("/api")
@@ -37,6 +36,17 @@ public class MyRestController {
     // Сейчас мы работаем с GET запросом и можем просто прописать его в браузере, в адресной строке.
     // Однако вспомним, что существуют еще и POST запросы и их параметры передаются в теле
     // запроса, в браузере мы уже так сделать не сможем. Поэтому скачаем небольшую программу
-    // Postman, она позволяет формировать запросы которые мы будем отправлять.
+    // Postman, она позволяет формировать запросы которые мы будем отправлять./
+
+    // В request аннотацию мы передаем путь в таком формате: /employees/{id}, то что мы указываем
+    // в фигурных скобках называется PathVariable, получить это значение можно указав параметром
+    // метода аннотацию @PathVariable, куда мы передаем тип данных переменной и ее название.
+    // Название должно совпадать с тем что мы указали в фигурных скобках, иначе ide не поймет
+    // откуда брать значение. Внутри метода все, как обычно, вызываем метод, который предоставляет
+    // нам одного работника по его id, а jackson переводит это все в json./
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return service.getEmployee(id);
+    }
 
 }
